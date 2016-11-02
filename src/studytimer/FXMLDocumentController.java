ff7f73a56974e8e105fc3392fac44f0195361611
Chris Andrews <chris.chase.andrews@gmail.com>
2016-11-02 17:51:00 -0600

    Fix the log entries, will only be one per day, will update if multiple sessions in one day

--- src/studytimer/FXMLDocumentController.java
+++ src/studytimer/FXMLDocumentController.java
@@ -16,10 +16,14 @@ import javafx.scene.control.Button;
 import java.util.Scanner;
 import java.io.File;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import javafx.animation.KeyFrame;
 
 import javafx.animation.Timeline;
@@ -57,6 +61,7 @@ public class FXMLDocumentController implements Initializable {
     
     private String sessionDate;
     
+    //this is iff, depending on where the "current directory" is ... ok for me
     private final String logFile = 
             "./timerdata/log file.txt";
             //"C:\\Users\\Chris\\Desktop\\Program\\TimerData\\log file.txt";
@@ -80,7 +85,7 @@ public class FXMLDocumentController implements Initializable {
             startBtn.requestFocus();
         }
         else if(event.getSource() ==resetBtn){
-            writeFiles("Timer Reset, " + this.sessionDate + " - " + 
+            writeFiles("\nTimer Reset, " + this.startFormated + " - " + 
                     this.getDate() + ", " + this.totalFormated + "\n");
             sessionTime = 0;
             totalTime = 0;
@@ -89,19 +94,108 @@ public class FXMLDocumentController implements Initializable {
         }
         else if (event.getSource()==closeBtn){
             writeFiles("");
-            
             Platform.exit();
         }
     }
     
-    public void writeFiles(String msg) throws IOException{
-        writeLog = new FileWriter(logFile, true);
-        if (sessionTime > 0)
+    public int parseTimeFromLog()throws IOException{
+        
+        String lastLine = getLastLoggedLine();
+        int previousTime = 0;
+        
+        if (lastLine.contains("Duration")){
+            System.out.println("It has duration");
+            //I need to now read the data
+            String[] fileInputs = lastLine.split(":");
+            String[] numbers = fileInputs[2].split("[a-z ]+");
+            /*
+            for (String s : numbers)
+                System.out.println(s); 
+            System.out.println("Size: " + numbers.length);
+            */
+            previousTime += Integer.parseInt(numbers[1]) * 3600;
+            previousTime += Integer.parseInt(numbers[2]) * 60;
+            previousTime += Integer.parseInt(numbers[3]);
+        }
+        return previousTime;
+    }
+    
+    public String parseDateFromLog() throws IOException{
+        String lastLoggedLine = getLastLoggedLine();
+        String lastLoggedDate = "";
+        
+        if (lastLoggedLine.contains("Duration"))
+            lastLoggedDate = lastLoggedLine.substring(6, 17);
+        return lastLoggedDate;
+    }
+    
+    public String getLastLoggedLine() throws IOException{
+        BufferedReader readLog = new BufferedReader(new FileReader(logFile));
+        String lastLine = "";
+        String temp;
+        
+        for (;;)
         {
-            writeLog.write("Date: " + this.sessionDate + "\tDuration: "
+            temp = readLog.readLine();
+            if (temp == null)
+                break;
+            else
+                lastLine = temp;
+        }
+        return lastLine;
+    }
+    
+    public void readLogIntoList(List<String> allLines) throws IOException { 
+            
+        String temp;
+        
+        BufferedReader readAll = new BufferedReader
+                    (new FileReader(logFile));
+        while (true){
+            temp = readAll.readLine();
+            if (temp == null)
+                break;
+            allLines.add(temp);
+        }
+    }
+    
+    public void writeFiles(String msg) throws IOException{
+        int sameDayTime = parseTimeFromLog();
+        //Now, if the above has anything
+        //updateFormatedTime();
+        writeLog = new FileWriter(logFile, true);
+        List<String> allLines = new ArrayList<String>();
+        String lastLoggedDate = this.parseDateFromLog();
+        System.out.println("LogTime: " + sameDayTime);
+        System.out.println("LogDate: " + lastLoggedDate);
+        System.out.println("SessionTime: " + sessionTime);
+        
+        if (lastLoggedDate.equals(this.getDate()) && sessionTime > 0)
+        {
+            readLogIntoList(allLines);
+        
+            if (sameDayTime > 0){
+                System.out.println("HERHEHREH");
+                sessionTime += sameDayTime;
+                updateFormatedTimes();
+                writeLog = new FileWriter(logFile, false);
+                for (int i = 0; i < allLines.size()-1; i++)
+                {
+                    System.out.println(allLines.get(i));
+                    writeLog.write(allLines.get(i) + "\n");
+                }
+                writeLog.write("Date: " + this.sessionDate + "\tDuration: "
                         + sessionFormated + "\n" + msg);
+                writeLog.close();
+            }
+        }
+        else if (sessionTime > 0)
+        {    
+            writeLog.write("Date: " + this.sessionDate + "\tDuration: "
+                    + sessionFormated + "\n" + msg);
             writeLog.close();
-        }else
+        }
+        else
         {
             writeLog.write(msg);
             writeLog.close();
@@ -110,8 +204,6 @@ public class FXMLDocumentController implements Initializable {
         writeTime.write(totalTime.toString()+"\n");
         writeTime.write(startFormated);
         writeTime.close();
-        
-        
     }
     
     @Override
@@ -131,6 +223,11 @@ public class FXMLDocumentController implements Initializable {
         totalTime = sessionTime + readInt;
         updateLabels();
         setupTimer();
+        try{
+            this.parseDateFromLog();
+        }catch (IOException e){
+            System.err.println(e);
+        }
     }
 
     public void setupTimer(){
@@ -155,13 +252,17 @@ public class FXMLDocumentController implements Initializable {
         return sdf.format(resultdate);
     }
     
-    public void updateLabels(){
+    public void updateFormatedTimes(){
         sessionFormated = "" + sessionTime/3600 + " hours "
             + (sessionTime/60)%60 + " minutes " + sessionTime%60
             + " seconds";
         totalFormated = "" + totalTime/3600 + " hours " 
             + (totalTime/60)%60 + " minutes " + totalTime%60
             + " seconds";    
+    }
+    
+    public void updateLabels(){
+        updateFormatedTimes();
         labelSession.setText(sessionFormated);
         labelTotal.setText(totalFormated);
         labelDate.setText(startFormated);
